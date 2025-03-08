8629bef438de1110953ff44d3d831c86e110fa51
Tetyana Solyanyk <tanyas81@gmail.com>
2025-03-08 04:41:43 +0200

    part 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import css from "./App.module.css";
+import initialContacts from "../../initialContacts.json";
+// import { nanoid } from "nanoid";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 import SearchBox from "../SearchBox/SearchBox.jsx";
 
 function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("saved-contacts");
+    console.dir(savedContacts);
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+  // const addContact = (newContact) => {
+  //   setContacts((prevContacts) => {
+  //     return [...prevContacts, newContact];
+  //   });
+  // };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
   return (
     <div>
       <h1 className={css.header}>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+      {/* <ContactForm /> */}
+      {/* <SearchBox /> */}
+      <ContactList contacts={contacts} deleteHandler={deleteContact} />
     </div>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,25 @@
 import css from "./Contact.module.css";
 
-function Contact() {
-  return <div className={css.container}>Contact</div>;
+function Contact({ contactInfo, onDelete }) {
+  return (
+    <li className={css.containerContact}>
+      <div className={css.fieldWrapper}>
+        <svg width={28} height={28}>
+          <use href="/src/img/sprite.svg#icon-user"></use>
+        </svg>
+        <p className={css.txtUsername}>{contactInfo.name}</p>
+      </div>
+      <div className={css.fieldWrapper}>
+        <svg width={28} height={28}>
+          <use href="/src/img/sprite.svg#icon-phone"></use>
+        </svg>
+        <p className={css.txtPhone}>{contactInfo.number}</p>
+      </div>
+      <button className={css.btnDelete} onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  );
 }
 
 export default Contact;
