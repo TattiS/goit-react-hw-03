b15d18a5afe42d6b18e6f1450fa022cca64cbc21
Tetyana Solyanyk <tanyas81@gmail.com>
2025-03-09 06:53:17 +0200

    step 4-6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 import css from "./App.module.css";
 import initialContacts from "../../initialContacts.json";
-// import { nanoid } from "nanoid";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 import SearchBox from "../SearchBox/SearchBox.jsx";
 
 function App() {
   const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-contacts");
-    console.dir(savedContacts);
-    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+    const contactsLS = window.localStorage.getItem("saved-contacts");
+    return contactsLS && JSON.parse(contactsLS).length > 0
+      ? JSON.parse(contactsLS)
+      : initialContacts;
   });
   useEffect(() => {
     window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
   }, [contacts]);
-  // const addContact = (newContact) => {
-  //   setContacts((prevContacts) => {
-  //     return [...prevContacts, newContact];
-  //   });
-  // };
+  const [filter, setFilter] = useState("");
+  let filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
 
   const deleteContact = (contactId) => {
     setContacts((prevContacts) => {
@@ -28,11 +32,11 @@ function App() {
   };
 
   return (
-    <div>
+    <div className={css.mainContainer}>
       <h1 className={css.header}>Phonebook</h1>
-      {/* <ContactForm /> */}
-      {/* <SearchBox /> */}
-      <ContactList contacts={contacts} deleteHandler={deleteContact} />
+      <ContactForm onAdd={addContact} />
+      <SearchBox inputValue={filter} setInputValue={setFilter} />
+      <ContactList contacts={filteredContacts} deleteHandler={deleteContact} />
     </div>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,19 +3,26 @@ import css from "./Contact.module.css";
 function Contact({ contactInfo, onDelete }) {
   return (
     <li className={css.containerContact}>
-      <div className={css.fieldWrapper}>
-        <svg width={28} height={28}>
-          <use href="/src/img/sprite.svg#icon-user"></use>
-        </svg>
-        <p className={css.txtUsername}>{contactInfo.name}</p>
+      <div className={css.infoContainer}>
+        <div className={css.fieldWrapper}>
+          <svg width={20} height={20}>
+            <use href="/src/img/sprite.svg#icon-user"></use>
+          </svg>
+          <p className={css.txtUsername}>{contactInfo.name}</p>
+        </div>
+        <div className={css.fieldWrapper}>
+          <svg width={20} height={20}>
+            <use href="/src/img/sprite.svg#icon-phone"></use>
+          </svg>
+          <p className={css.txtPhone}>{contactInfo.number}</p>
+        </div>
       </div>
-      <div className={css.fieldWrapper}>
-        <svg width={28} height={28}>
-          <use href="/src/img/sprite.svg#icon-phone"></use>
-        </svg>
-        <p className={css.txtPhone}>{contactInfo.number}</p>
-      </div>
-      <button className={css.btnDelete} onClick={onDelete}>
+      <button
+        className={css.btnDelete}
+        onClick={() => {
+          onDelete(contactInfo.id);
+        }}
+      >
         Delete
       </button>
     </li>
